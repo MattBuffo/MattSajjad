0b989a0bef1c84b66a92d40f07b7f1512c3ef7cd
SajjadShuaybi <32776191+SajjadShuaybi@users.noreply.github.com>
2017-10-19 12:10:32 -0400

    worked on location + zipcode
    kkk

--- src/Location.java
+++ src/Location.java
@@ -1,5 +1,48 @@
 
 public class Location {
-//hi
-//oh hi there
+
+	private String city;
+	private String state;
+	
+	public Location()
+	{
+		
+	}
+	
+	public Location(String city)
+	{
+		
+	}
+	
+	public Location(String state)
+	{
+		
+	}
+	
+	public Location(String location)
+	{
+		
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	public String toString()
+	{
+		return (city + ", " + state);
+	}
+	
 }
--- src/ZipCode.java
+++ src/ZipCode.java
@@ -1,4 +1,49 @@
 
 public class ZipCode {
 
+	private String zipCode;
+	private Location[] Place = new Location[25];
+	
+	public ZipCode()
+	{
+		
+	}
+	
+	public String getZipCode(String code)
+	{
+		
+	}
+	
+	public String getLocation()
+	{
+		
+	}
+	
+	public String convertToBarcode()
+	{
+		
+	}
+	
+	public String toString()
+	{
+		
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public Location[] getPlace() {
+		return Place;
+	}
+
+	public void setPlace(Location[] place) {
+		Place = place;
+	}
+
+
 }
