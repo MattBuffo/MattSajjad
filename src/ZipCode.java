ea6d5e637602ff4843b278b2559ad0da18490a6d
SajjadShuaybi <32776191+SajjadShuaybi@users.noreply.github.com>
2017-10-27 12:56:34 -0400

    commented zipcode

--- src/ZipCode.java
+++ src/ZipCode.java
@@ -1,5 +1,7 @@
 /**
  * @author Sajjad Shuaybi
+ * this class performs the zipcode class functions
+ * as per the project description
  * 
  */
 
@@ -15,7 +17,12 @@ public class ZipCode {
 	private String readableBarcode;
 	private File ZipCodesCity;
 	private int checkSum;
-
+/**
+ * constructor method
+ * @preconditions none
+ * @postconditions none
+ * @param code - zipcode object
+ */
 	public ZipCode(String code) 
 	{
 		
@@ -44,16 +51,22 @@ public class ZipCode {
 	}
 	 **/ //redundant method
 
+	/**
+	 * setter method that adds to the locations from the zipcodes
+	 * no return value, just modifies the place instance array
+	 */
 	private void setLocation() 
 	{
 		
 		try {
-			Scanner cityReader = new Scanner(ZipCodesCity);
+			Scanner cityReader = new Scanner(ZipCodesCity); //read in the file of locations
 		
 		for (int i = 0; i < place.length; i++ ){
 			boolean tempBool = false;
+			
 			while (cityReader.hasNextLine() && !(tempBool)) {
 				String location = cityReader.nextLine();
+				
 				if(location.substring(0, 5).equals(zipCode)) {
 					place[i] = new Location(location);
 					tempBool = true;
@@ -61,12 +74,16 @@ public class ZipCode {
 			}
 		}
 		cityReader.close();
-		} catch (FileNotFoundException e) {
+		} catch (FileNotFoundException e) { //fixes the scanner leak error
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-
+/**
+ * 
+ * @param n, n is each digit of the zipcode
+ * @return the matching barcode segment to be built later
+ */
 	public String convertToBarcode(int n)
 	{
 		switch (n) {
@@ -80,23 +97,34 @@ public class ZipCode {
 		case 7: return "|:::|";
 		case 8: return "|::|:";
 		case 9: return "|:|::";
-		default: return "Invalid Input";
+		default: return "Invalid Input"; //in case number passed in is not between 0-9
 
 		}
 	}
+	/**
+	 * modifies the checksum instance data
+	 * to be used for barcode construction
+	 * 
+	 */
 
 	private void getCheckSum() {
-		int sum = 0;
+		int sum = 0;	//add up all digits of zipcode
 		for(int i = 0; i<zipCode.length(); i++) {
 			sum += zipCode.charAt(i);
 		}
-		if (sum%10 != 0) {
+		if (sum%10 != 0) {		//checksum cannot be 10
 			checkSum = 10 - sum%10;
 		}
 		else {
 			checkSum = 0;
 		}
 	}
+	
+	/**
+	 * constructs the barcode using the segments and checksum
+	 * @preconditons zipcode must be declared and initialized
+	 * @postconditions no return value, modifies barcode instance data
+	 */
 
 	public void setBarcode() {
 		for(int i = 0; i<zipCode.length(); i++) {
@@ -104,28 +132,58 @@ public class ZipCode {
 		}
 		barcode += convertToBarcode(checkSum);
 	}
+	
+	/**
+	 * getter method for barcode object
+	 * @preconditions barcode object must be instantiated
+	 * @returns initalized barcode
+	 */
+	
 	public String getBarcode() {
 		return barcode;
 	}
+	
+	/**
+	 * @preconditions barcode object must be intstantiated and initialized
+	 * setter method that creates a readable barcode
+	 * @postconditions no return method, modifies readablebarcode instance data
+	 * 
+	 */
 	private void setReadableBarcode() {
 		readableBarcode = "";
 		for(int i = 0; i<barcode.length(); i+=5) {
 			
-			readableBarcode += barcode.substring(i , i+5) + " ";
+			readableBarcode += barcode.substring(i, i+5) + " "; //adds space after each segment
 		}
 	}
+	/**
+	 * setter method sets zipcode to external zipcode string
+	 * @param zipCode
+	 */
 
 	public void setZipCode(String zipCode) {
 		this.zipCode = zipCode;
 	}
+	/**
+	 * getter method 
+	 * @return array of places
+	 */
 
 	public Location[] getPlace() {
 		return place;
 	}
+	/**
+	 * setter method sets place to external place array
+	 * @param place
+	 */
 
 	public void setPlace(Location[] place) {
 		this.place = place;
 	}
+	/**
+	 * tostring method
+	 * prints all the project requirements with correct formatting
+	 */
 
 	public String toString()
 	{
